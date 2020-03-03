6685ebbd74ec5328f6fd7bfa760a3663efe034da
Alfonso Gil <algparis96@gmail.com>
2020-03-03 11:53:08 +0100

    Activity nuevo creado (vacío)

--- app/src/main/java/com/tfgstuff/blueapp/MainActivity.java
+++ app/src/main/java/com/tfgstuff/blueapp/MainActivity.java
@@ -1,17 +1,23 @@
 package com.tfgstuff.blueapp;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,15 +25,12 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
 
     public final static String TAG = MainActivity.class.getSimpleName();
-
+    public static final int REQUEST_ACCESS_COARSE_LOCATION = 11;
     public static final int REQUEST_ENABLE_BT = 1;
-
     private HashMap<String, BTLE_Device> mBTDevicesHashMap;
     private ArrayList<BTLE_Device> mBTDevicesArrayList;
     private ListAdapter_BTLE_Devices adapter;
-
     private Button btn_scan;
-
     private BroadcastReceiver_BTState mBTStateUpdateReceiver;
     private Scanner_BTLE mBTLeScanner;
 
@@ -35,28 +38,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mBTStateUpdateReceiver = new BroadcastReceiver_BTState(getApplicationContext());
         mBTLeScanner = new Scanner_BTLE(this, 7500, -75);
-
         mBTDevicesHashMap = new HashMap<>();
         mBTDevicesArrayList = new ArrayList<>();
-
         adapter = new ListAdapter_BTLE_Devices(this, R.layout.btle_device_list_item, mBTDevicesArrayList);
-
         ListView listView = new ListView(this);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
         ((ScrollView) findViewById(R.id.scrollView)).addView(listView);
-
         btn_scan = (Button) findViewById(R.id.btn_scan);
         findViewById(R.id.btn_scan).setOnClickListener(this);
+
+        checkCoarseLocationPermission();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
         registerReceiver(mBTStateUpdateReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
     }
 
@@ -68,14 +67,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onPause() {
         super.onPause();
-
         stopScan();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         unregisterReceiver(mBTStateUpdateReceiver);
         stopScan();
     }
@@ -90,54 +87,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_ENABLE_BT) {
             if (resultCode == RESULT_OK) {
-
             } else if (resultCode == RESULT_CANCELED) {
                 Utils.toast(getApplicationContext(), "Por favor, habilita el Bluetooth");
             }
         }
     }
 
-    @Override
-    public void onClick(View v) {
+    private boolean checkCoarseLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_ACCESS_COARSE_LOCATION);
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-        switch (v.getId()) {
-            case R.id.btn_scan:
-                if (!mBTLeScanner.isScanning()) {
-                    startScan();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int [] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+
+        switch (requestCode){
+            case  REQUEST_ACCESS_COARSE_LOCATION:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,"Permiso concedido. Puedes escanear.",Toast.LENGTH_LONG).show();
                 } else {
-                    stopScan();
+                    Toast.makeText(this,"Permiso denegado. No puedes escanear.",Toast.LENGTH_LONG).show();
                 }
-
-                break;
-            default:
                 break;
         }
 
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_scan:
+                if (checkCoarseLocationPermission()){
+                    if (!mBTLeScanner.isScanning()) {
+                        startScan();
+                    } else {
+                        stopScan();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
-
     }
 
     public void addDevice(BluetoothDevice device, int new_rssi) {
-
         String address = device.getAddress();
         if (!mBTDevicesHashMap.containsKey(address)) {
             BTLE_Device btle_device = new BTLE_Device(device);
             btle_device.setRssi(new_rssi);
-
             mBTDevicesHashMap.put(address, btle_device);
             mBTDevicesArrayList.add(btle_device);
         } else {
             mBTDevicesHashMap.get(address).setRssi(new_rssi);
         }
-
         adapter.notifyDataSetChanged();
     }
 
